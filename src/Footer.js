327b90284818508b5ca10ccf2fe7c92f8c5064c0
Phillip Andersen <phillip_andersen@hotmail.com>
2020-12-10 13:57:37 +0100

    footerfærdig

--- src/Footer.js
+++ src/Footer.js
@@ -1,55 +1,40 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-function Footer() {
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const FooterPage = () => {
   return (
-    <footer class="page-footer font-small cyan darken-3">
-      <div class="container">
-        <div class="row">
-          <div class="c</footer>ol-md-12 py-5">
-            <div class="mb-5 flex-center">
-              <a class="fb-ic">
-                <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x">
-                  {" "}
-                </i>
-              </a>
-
-              <a class="tw-ic">
-                <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
-                  {" "}
-                </i>
-              </a>
-
-              <a class="gplus-ic">
-                <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x">
-                  {" "}
-                </i>
-              </a>
-
-              <a class="li-ic">
-                <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
-                  {" "}
-                </i>
-              </a>
-
-              <a class="ins-ic">
-                <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
-                  {" "}
-                </i>
-              </a>
-
-              <a class="pin-ic">
-                <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
-              </a>
-            </div>
-          </div>
-        </div>
+    <MDBFooter color="green" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Made by kernen</h5>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Link 1</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="#"> Kernen </a>
+        </MDBContainer>
       </div>
-
-      <div class="footer-copyright text-center py-3">
-        © 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-      </div>
-    </footer>
+    </MDBFooter>
   );
-}
+};
 
-export default Footer;
+export default FooterPage;
